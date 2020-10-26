c2118452b3af9a671d27bb14f6ecfc8030a678ed
JY-5 <58899117+JY-5@users.noreply.github.com>
2020-10-25 22:59:47 -0700

    Finished css assignment

--- src/ArticleList/ArticleList.jsx
+++ src/ArticleList/ArticleList.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import PropTypes from "prop-types";
 import ArticleListItem from "../ArticleListItem/ArticleListItem";
+import styles from "./ArticleList.module.css";
 
 const ArticleList = (props) => {
     return (
--- src/ArticleListItem/ArticleListItem.jsx
+++ src/ArticleListItem/ArticleListItem.jsx
@@ -1,14 +1,36 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useState } from "react";
+import ArticleTextToggleButton from "../ArticleTextToggleButton/ArticleTextToggleButton";
+import styles from "./ArticleListItem.module.css";
 
 const ArticleListItem = (props) => {
+    const [ifClicked, setClicked] = useState(false);
+
+    function changeClick() {
+      setClicked(!ifClicked);
+    }
+
+    let textOnButton;
+    let displayContent;
+    if (!ifClicked) {
+      textOnButton = "Show more";
+      displayContent = null;
+    } else {
+      textOnButton = "Show less";
+      displayContent = (
+        <div className="display-content"> 
+          <p>{props.article.shortText}</p>
+          <p><time dateTime={props.article.timeStamp}>{props.article.displayDate}</time></p>
+        </div>
+      );
+    }
+
     return (
       <section>
         <header>
           <h1>{props.article.title}</h1>
         </header>
-        <time dateTime={props.article.timeStamp}>{props.article.displayDate}</time>
-        <p>{props.article.shortText}</p>
+        {displayContent}
+        <ArticleTextToggleButton buttonText={textOnButton} onClick={changeClick}/>
       </section>
     );
 };
